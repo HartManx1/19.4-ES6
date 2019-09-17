ae80aa4f903b77b58062407a4cd79c2def483ebf
Martin Hartman <hartmanx1@wp.pl>
2019-09-17 17:03:05 +0200

    19.4 edit script

--- script-compiled.js
+++ script-compiled.js
@@ -1,29 +1,44 @@
+'use strict';
+
 //1
 
-const string1 = 'Hello';
-const string2 = 'World!';
+var string1 = 'Hello';
+var string2 = 'World!';
 
-const sayHello =`${string1} ${string2}`
-console.log(`${sayHello}`)
+var sayHello = string1 + ' ' + string2;
+console.log('' + sayHello);
 
 //2
 
-let multiply = (x, y = 1) => (x * y);
+var multiply = function multiply(x) {
+  var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return x * y;
+};
 console.log(multiply(2));
 
 //3
 
-const average = (...data) => data.reduce((a, b) => a + b) / data.length;
+var average = function average() {
+  for (var _len = arguments.length, data = Array(_len), _key = 0; _key < _len; _key++) {
+    data[_key] = arguments[_key];
+  }
+
+  return data.reduce(function (a, b) {
+    return a + b;
+  }) / data.length;
+};
 console.log(average(1, 5, 12, 18));
 
 //4
 
-const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
-console.log(average(...grades));
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+console.log(average.apply(undefined, grades));
 
 //5
 
-const data = [1, 4, 'Iwona', false, 'Nowak']
-const [, , firstname, , lastname] = data;
+var data = [1, 4, 'Iwona', false, 'Nowak'];
+var firstname = data[2],
+    lastname = data[4];
 
-console.log(`${firstname} ${lastname}`);
+
+console.log(firstname + ' ' + lastname);
--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-const sayHello = () => alert('Hello world!');
-sayHello();
+//1
+
+const string1 = 'Hello';
+const string2 = 'World!';
+
+const sayHello =`${string1} ${string2}`
+console.log(`${sayHello}`)
+
+//2
+
+let multiply = (x, y = 1) => (x * y);
+console.log(multiply(2));
+
+//3
+
+const average = (...data) => data.reduce((a, b) => a + b) / data.length;
+console.log(average(1, 5, 12, 18));
+
+//4
+
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+console.log(average(...grades));
+
+//5
+
+const data = [1, 4, 'Iwona', false, 'Nowak']
+const [, , firstname, , lastname] = data;
+
+console.log(`${firstname} ${lastname}`);
